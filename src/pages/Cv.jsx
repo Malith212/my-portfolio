7714e536bcbb5d327b3461effba3e572d6754373
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-22 21:42:27 +0530

    certifications and the achivments

--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -9,23 +9,22 @@ import ExtracurricularActivities from "../components/cv/Extracurricular";
 import Footer from "../components/Footer";
 
 function Cv() {
-    return (
-      <div className="font-sans">
-        <Navbar />
-        <section className="container mx-auto py-10 px-4">
-          <DownloadCV />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <Education />
-            <ExtracurricularActivities />
-            <Certificates />
-            
-            <Languages />
-            <Achievements />
-          </div>
-        </section>
-        <Footer />
-      </div>
-    );
-  }
-  
-  export default Cv;
+  return (
+    <div className="font-sans">
+      <Navbar />
+      <section className="container mx-auto py-10 px-4">
+        <DownloadCV />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <Education />
+          <ExtracurricularActivities />
+          <Certificates />
+          <Achievements />
+          <Languages />
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Cv;
