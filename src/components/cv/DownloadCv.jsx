79ee06b11b4440d4606134620156ae4cd0b4d3fb
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-23 12:25:04 +0530

    download cv button updated in cv section

--- src/components/cv/DownloadCv.jsx
+++ src/components/cv/DownloadCv.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const DownloadCv = () => {
   return (
-    <section className="py-10 text-right mt-20"> {/* Add mt-20 or similar value */}
+    <section className="py-10 text-right mt-20">
       <a
         href="/path-to-your-cv.pdf" // Make sure to replace with the correct path
         download
-        className="bg-gradient-to-r from-[#141E30] to-[#243B55] text-white py-2 px-4 rounded hover:bg-[#2D3436] transition-colors duration-300"
+        className="inline-block bg-gradient-to-r from-[#141E30] to-[#243B55] text-white py-3 px-6 rounded-lg shadow-lg hover:from-[#0F2027] hover:to-[#2C5364] transition-all duration-300 ease-in-out transform hover:-translate-y-1"
       >
         Download CV
       </a>
