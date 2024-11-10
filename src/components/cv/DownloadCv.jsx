4b4de9f8edfdc2c5720d9eda6412bfe11c26c578
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-11-10 09:23:56 +0530

    comment removed in downloadcv.jsx file

--- src/components/cv/DownloadCv.jsx
+++ src/components/cv/DownloadCv.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import CV from '../../assets/cv.pdf'; // Import the PDF file
+import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const DownloadCv = () => {
   return (
     <section className="py-10 text-right mt-20">
       <a
-        href={CV} // Use the imported CV file for the href
-        download="Malith_Weerarathne_CV.pdf" // You can specify a name for the downloaded file
+        href={CV}
+        download="Malith_Weerarathne_CV.pdf" 
         className="inline-block bg-gradient-to-r from-[#141E30] to-[#243B55] text-white py-3 px-6 rounded-lg shadow-lg hover:from-[#0F2027] hover:to-[#2C5364] transition-all duration-300 ease-in-out transform hover:-translate-y-1"
       >
         Download CV
